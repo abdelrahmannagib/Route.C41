974b43b008255fdca40ac7f991b5d158c67690d8
x x <20201700449@cis.asu.edu.eg>
2024-03-20 14:20:40 +0200

    Part 03 3-Layers Arch. Pattern - Data Access Layer

---
+++ Route.C41.DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Route.C41.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.C41.DAL.Data
+{
+	internal class ApplicationDbContext: DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Server = localhost\\sqlexpress; Database = MVCS01; Trusted_Connection = True; TrustServerCertificate=true;");
+		}
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+		}
+		public DbSet<Department> Departments { get; set; }
+	}
+}
