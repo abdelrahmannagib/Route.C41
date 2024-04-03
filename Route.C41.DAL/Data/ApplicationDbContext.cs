e0a2d081dda42c21bde0e5684e106168c53260f0
x x <20201700449@cis.asu.edu.eg>
2024-04-03 11:23:48 +0200

    Microsoft Identity Package

--- Route.C41.DAL/Data/ApplicationDbContext.cs
+++ Route.C41.DAL/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Route.C41.DAL.Models;
 using System;
 using System.Collections.Generic;
@@ -9,21 +10,22 @@ using System.Threading.Tasks;
 
 namespace Route.C41.DAL.Data
 {
-	public class ApplicationDbContext: DbContext
+	public class ApplicationDbContext: IdentityDbContext
 	{
 		public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
 		{
 
 		}
-		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-		{
-            //optionsBuilder.UseSqlServer();
-           // optionsBuilder.UseSqlServer("Server = localhost\\sqlexpress; Database = ITI_DB; Trusted_Connection = True; TrustServerCertificate=true;");
-
-        }
+		///protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		///{
+		///          //optionsBuilder.UseSqlServer();
+	    ///         // optionsBuilder.UseSqlServer("Server = localhost\\sqlexpress; Database = ITI_DB; Trusted_Connection = True; TrustServerCertificate=true;");
+		///
+		///      }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
+			base.OnModelCreating(modelBuilder);
 			modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
 		}
 		public DbSet<Department> Departments { get; set; }
