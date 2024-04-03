2ce764d49f82f3a57c05016fe201966397cec163
x x <20201700449@cis.asu.edu.eg>
2024-04-03 12:13:18 +0200

    Account Controller - SignUp

---
+++ Route.C41.PL/ViewModels/User/SignupViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Route.C41.PL.ViewModels.User
+{
+	public class SignupViewModel
+	{
+		public string UserName { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Email { get; set; }
+		[DataType(DataType.Password)]
+		[Required]
+		public string Passoword { get; set; }
+		[DataType(DataType.Password)]
+		[Required]
+		[Compare(nameof(Passoword), ErrorMessage = "Password Not Matched")]
+		public string ConfirmPassword { get; set; }
+		public bool IsAgree { get; set; }
+	}
+}
